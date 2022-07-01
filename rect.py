64a88383d87b0030908a4a7a245da1612777416d
Anjana <66191342+anj-ana@users.noreply.github.com>
2022-07-01 13:07:42 -0700

    area-perimeter

---
+++ rect.py
@@ -0,0 +1,20 @@
+
+
+
+class Rectangle:
+    def __init__(self,length,width) -> None:
+        self.len=length
+        self.wid=width
+    def Perimeter(self):
+        return 2*((self.len)+(self.wid))
+    def Area(self):
+        
+        return (self.len)*(self.wid)
+    def Display(self):
+        print("rectlen:",self.len)
+        print("rectwid:",self.wid)
+        print("rectperi:",self.Perimeter())
+        print("rectarea:",self.Area())
+r=Rectangle(3,5)
+r.Display()
+        
