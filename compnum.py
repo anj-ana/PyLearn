e5667acabbe6a6f252f24b8dfc6b5bd09badbd1e
Anjana <66191342+anj-ana@users.noreply.github.com>
2022-07-01 12:05:25 -0700

    parametrized

--- compnum.py
+++ compnum.py
@@ -6,3 +6,15 @@ class Compnum:
         print("The formula is:",formula)    
 c=Compnum()
 c.ShowNum("a+ib")
+
+//parametrised
+
+class Compnum:
+    def __init__(self,r=0,i=0) -> None:
+        self.real=r
+        self.imag=i
+        
+    def ShowNum(self):
+        print(self.real+(self.imag))    
+c=Compnum(2,4)
+c.ShowNum()
