10881b450a76a80e5496ea0dff65465f1ad4dfac
Anjana <66191342+anj-ana@users.noreply.github.com>
2022-07-02 07:03:12 -0700

    second

--- GetSet.py
+++ GetSet.py
@@ -10,3 +10,18 @@ p=Person("Anju","Sree")
 print("NAME1:",p.getName())
 p.setName("Anu")
 print("Name2:",p.getName())
+
+//bankAccount
+
+class Bank:
+    def __init__(self,name,branch) -> None:
+        self.Name=name
+        self.Branch=branch
+    def setName(self,name):
+        self.Name=name
+    def getName(self):
+        return self.Name
+b=Bank("Anju","Kozhikode")
+print(b.getName())
+b.setName("Zadu")
+print(b.getName())
